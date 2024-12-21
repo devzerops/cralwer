d0e23ee2ec0a81408b12206bd5c0b975121dba08
devzerops <96872399+devzerops@users.noreply.github.com>
2024-12-21 06:10:37 +0000

    colly library add, and crawler logic add

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,8 +1,10 @@
 package crawler
 
 import (
-    //"distributed-crawler/models"
+    "distributed-crawler/models"
     "distributed-crawler/storage"
+    "github.com/gocolly/colly/v2"
+    "log"
 )
 
 type Crawler struct {
@@ -14,5 +16,17 @@ func NewCrawler(storage storage.Storage) *Crawler {
 }
 
 func (c *Crawler) Crawl(url string) {
-    // Implement crawling logic here
+    // Create a new collector
+    collector := colly.NewCollector()
+
+    // On every a element which has href attribute call callback
+    collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+        link := e.Attr("href")
+        log.Printf("Found link: %s", link)
+        // Save the link to storage
+        c.Storage.Save(models.URL{Address: link, Priority: 1})
+    })
+
+    // Start scraping the URL
+    collector.Visit(url)
 }
