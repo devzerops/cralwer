f0f40b809700b2b22a614be0753f12c3115dffce
devzerops <96872399+devzerops@users.noreply.github.com>
2024-12-21 07:11:17 +0000

    URL parsing directory problem logic edit

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -5,6 +5,7 @@ import (
     "distributed-crawler/storage"
     "github.com/gocolly/colly/v2"
     "log"
+    "net/url"
 )
 
 type Crawler struct {
@@ -15,18 +16,32 @@ func NewCrawler(storage storage.Storage) *Crawler {
     return &Crawler{Storage: storage}
 }
 
-func (c *Crawler) Crawl(url string) {
+func (c *Crawler) Crawl(startURL string) {
     // Create a new collector
     collector := colly.NewCollector()
+
+    // Parse the start URL
+    baseURL, err := url.Parse(startURL)
+    if err != nil {
+        log.Fatalf("Invalid start URL: %v", err)
+    }
 
     // On every a element which has href attribute call callback
     collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
         link := e.Attr("href")
-        log.Printf("Found link: %s", link)
+        // Parse the link URL
+        linkURL, err := url.Parse(link)
+        if err != nil {
+            log.Printf("Invalid link URL: %v", err)
+            return
+        }
+        // Resolve the link URL to an absolute URL
+        absoluteURL := baseURL.ResolveReference(linkURL).String()
+        log.Printf("Found link: %s", absoluteURL)
         // Save the link to storage
-        c.Storage.Save(models.URL{Address: link, Priority: 1})
+        c.Storage.Save(models.URL{Address: absoluteURL, Priority: 1})
     })
 
     // Start scraping the URL
-    collector.Visit(url)
+    collector.Visit(startURL)
 }
